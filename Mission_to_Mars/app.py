250f0b7319270b096c90255c14f33793fe4dab58
Lisa Harrison <lmharrison0810@gmail.com>
2020-09-28 15:25:58 -0400

    app.py file updated

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -13,15 +13,15 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars_data = mongo.db.mars_scraped.find_one()
+    mars_data = mongo.db.mars_scrapedit.find_one()
     return render_template("index.html", mars_data=mars_data)
 
 @app.route("/scrape")
 def scrape():
 
-    mars_scraped = mongo.db.mars_scraped
+    mars_scrapedit = mongo.db.mars_scrapedit
     mars_data = scrape_mars.scrape()
-    mongo.db.mars_scraped.update({}, mars_data, upsert=True)
+    mongo.db.mars_scrapedit.update({}, mars_data, upsert=True)
     return redirect('/', code=302)
 
 if __name__ == "__main__":
